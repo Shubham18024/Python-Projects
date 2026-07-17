a623c6177fcfc11c960156479df920231d935e00
Shubham Tiwari <shubh.tiwari2244@gmail.com>
2026-07-17 11:19:24 +0530

    Clarify tip input prompt to include percentage

--- Simple_Python_Projects/Bill_divider.py
+++ Simple_Python_Projects/Bill_divider.py
@@ -1,6 +1,6 @@
 print("Welcome to the tip calculator!")
 bill=float(input("What was the total bill? $\t"))
-tip=int(input("How much tip would you like to give? 10, 12, or 15?\t"))
+tip=int(input("How much tip would you like to give? 10, 12, or 15? (It's in %)\t"))
 person=int(input("How many people to split the bill?\t"))
 new_bill=((tip/100)*bill)+bill
 pay_per_person=new_bill/person
